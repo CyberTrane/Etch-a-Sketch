343dc98700cefb1bc0f95cae0f4e886e456cf235
CyberTrane <58540182+CyberTrane@users.noreply.github.com>
2023-02-12 18:32:02 -0500

    Added event to each square div that highlights it
    A function to color the square was made for the mouse event on each of
    the square divs.

--- script.js
+++ script.js
@@ -3,14 +3,22 @@ const container = document.querySelector('.container');
 function createGrid(size) {
     for (let i = 1; i <= size; i++) {
         const row = document.createElement('div');
-        row.classList.add('row' + i);
+        row.classList.add('row');
         for (let j = 1; j <= size; j++) {
             const square = document.createElement('div');
-            square.classList.add('square' + j);
+            square.classList.add('square');
             row.appendChild(square);
         }
         container.appendChild(row);
     }
 }
 
+function colorGrid(e) {
+    this.classList.add('colored');
+    
+}
+
 createGrid(16);
+
+const squares = Array.from(document.querySelectorAll('.square'));
+squares.forEach(square => square.addEventListener('mouseenter', colorGrid));
