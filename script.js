2eb25d95efee8d2860cd2b6788bde4aa7797e394
CyberTrane <58540182+CyberTrane@users.noreply.github.com>
2023-02-12 20:01:31 -0500

    Added listener to button which changes grid size
    Button triggers function which prompts user for grid size and checks
    if input is valid before calling gridCreate function. Also changed some
    css for squares.

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const container = document.querySelector('.container');
 
 function createGrid(size) {
+    container.innerHTML = '';
     for (let i = 1; i <= size; i++) {
         const row = document.createElement('div');
         row.classList.add('row');
@@ -11,19 +12,43 @@ function createGrid(size) {
         }
         container.appendChild(row);
     }
+
+    const squares = Array.from(document.querySelectorAll('.square'));
+    squares.forEach(square => square.addEventListener('mouseover', colorGrid));
+}
+
+function userInput() {
+    let input = prompt('Enter New Grid Size');
+    while (true) {
+        if (!(Number(input))) {
+            input = prompt('Not a number, try again');
+        }
+        else if (input > 64) {
+            input = prompt('Size limit is 64, try again');
+        } else if (input < 1) {
+            input = prompt('Size cannot be less than 1, try again');
+        } else {
+            createGrid(input);
+            break;
+        }
+    }
+    
 }
 
 function colorGrid(e) {
     this.classList.add('coloring');
-    
 }
 
 createGrid(16);
 
-const squares = Array.from(document.querySelectorAll('.square'));
-squares.forEach(square => square.addEventListener('mousedown', () => {
-    squares.forEach(square => square.addEventListener('mouseover', colorGrid));
+/* const squares = Array.from(document.querySelectorAll('.square'));
+squares.forEach(square => square.addEventListener('mouseover', colorGrid)); */
+/* squares.forEach(square => square.addEventListener('mousedown', () => {
+    squares.forEach(square => square.addEventListener('mousemove', colorGrid));
 }));
 squares.forEach(square => square.addEventListener('mouseup', () => {
-    squares.forEach(square => square.removeEventListener('mouseover', colorGrid));
-}));
+    squares.forEach(square => square.removeEventListener('mousemove', colorGrid));
+})); */
+
+const button = document.querySelector('button');
+button.addEventListener('click', userInput);
